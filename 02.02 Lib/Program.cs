e8f3779eec79aab2278af79e4a5f9926e9619ccd
T4emp <sergey63777@mail.ru>
2022-01-26 14:38:34 +0300

    Добавьте файлы проекта.

---
+++ 02.02 Lib/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Geometrus.Sergey;
+
+namespace _02._02_Lib
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 class1 = new Class1();
+            Class1.Plosh();
+            Class1.Chislo();
+        }
+    }
+}
---
+++ Geometrus.Sergey/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrus.Sergey
+{
+    public class Class1
+    {
+        public static void Plosh()
+        {
+            Console.WriteLine("Введите сторону квадрата: ");
+            string a2 = Console.ReadLine();
+            int b2 = Convert.ToInt32(a2);
+            Console.WriteLine($"Площадь квадрата: {b2*b2}\nОбъем квадрата: {b2*b2*b2}");
+        }
+        public static void Chislo()
+        {
+            Console.WriteLine("Введите число: ");
+            string a2 = Console.ReadLine();
+            int b2 = Convert.ToInt32(a2);
+            Console.WriteLine($"Итог: {(b2*b2*b2)/2}");
+        }
+    }
+}
